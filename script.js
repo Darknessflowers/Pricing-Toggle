977836bd4919e3f66a3fc9f486d755ff1e80edc6
Darknessflowers <darknessflowers@hotmail.co.uk>
2020-07-18 18:39:47 +1200

    Styling

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('it works');
+const plan = document.querySelector('.switch');
+const monthly = Array.from(document.querySelectorAll('.monthlyPrice'));
+const annual = document.querySelectorAll('.annualPrice');
+const price = document.querySelectorAll('.price');
+
+
+function switcher() {
+  console.log('switching plan');
+//when button is clicked remove 'hide from monthly and add to annual
+monthly.forEach(function(item) {
+  if(item.classList.contains('hide')) {
+    item.classList.remove('hide');
+  } else {
+    item.classList.add('hide');
+  }
+});
+  annual.forEach(function(item) {
+    if(!item.classList.contains('hide')) {
+      item.classList.add('hide');
+    } else {
+      item.classList.remove('hide');
+    }
+  });
+}
+
+plan.addEventListener('click', switcher);
